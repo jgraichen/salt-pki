df87edcdf58d6c33a3b0235e87732efe88b62e01
Jan Graichen <jgraichen@altimos.de>
2026-09-14 22:16:01 +0200

    chore!: Update testing to only cover current LTS onedirs
    1. Drop 3007, which is broken and cannot be installed correctly. It's
       non-LTS and EOL too.
    
    2. Update the Python and cryptography versions to match the current LTS
       onedirs. Apparently, they changed them on minor updates.

---
+++ noxfile.py
@@ -0,0 +1,40 @@
+import nox
+from nox import Session
+
+nox.options.default_venv_backend = "uv"
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session
+@nox.parametrize(
+    "python,salt,cryptography",
+    [
+        # Python and cryptography versions as shipped in salt's onedir
+        # packages, to test against what users actually run.
+        nox.param("3.11", 3006, 47, id="salt3006"),
+        nox.param("3.14", 3008, 48, id="salt3008"),
+    ],
+)
+def test(s: Session, salt: int, cryptography: int) -> None:
+    s.install(
+        "--verbose",
+        ".",
+        f"salt~={salt}.0",
+        f"cryptography~={cryptography}.0",
+        "--group=test",
+    )
+
+    s.run("uv", "pip", "list")
+
+    s.run("salt", "--versions-report")
+    s.run("pytest", *s.posargs)
+    s.run("pylint", "_modules", "_states")
+
+
+@nox.session
+def lint(s: Session) -> None:
+    s.install("--group=lint")
+    s.run("uv", "pip", "list")
+
+    s.run("ruff", "check", "--diff", "--no-fix", ".")
+    s.run("ruff", "format", "--diff", "--check", ".")
